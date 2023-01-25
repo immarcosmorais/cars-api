6e9f6aaed9ea3dabdfdcb04f3ec6036580800b01
Marcos Morais <marcosmorais.contact@gmail.com>
2023-01-25 16:54:51 -0300

    master Created method to get the brands with more modeld

--- routes/cars.js
+++ routes/cars.js
@@ -1,8 +1,33 @@
 import express from "express";
 const router = express.Router();
+import { promises as fs } from "fs";
+const { readFile } = fs;
 
-router.get("/", async (req, res, next) => {
-  res.end();
+router.get("/brandWithMoreModels", async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile(fileName));
+    let results = [];
+    data.sort((a, b) => b.models.length - a.models.length);
+    data.forEach((record) => {
+      if (results.length === 0) {
+        results.push(record);
+      } else {
+        results.forEach((result) => {
+          if (result.models.length === record.models.length) {
+            results.push(record);
+          }
+        });
+      }
+    });
+    res.send(results);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use((err, req, res, next) => {
+  logger.error(`${req.method} ${req.baseUrl} - ${err.message}`);
+  res.status(400).send({ error: err.message });
 });
 
 export default router;
