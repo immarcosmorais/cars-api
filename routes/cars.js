afa03aaf57d1be80a0b34bac936f8c42201045e6
Marcos Morais <marcosmorais.contact@gmail.com>
2023-01-25 17:13:10 -0300

    master Created method to get the brands with less models

--- routes/cars.js
+++ routes/cars.js
@@ -29,6 +29,32 @@ router.get("/brandWithMoreModels", async (req, res, next) => {
   }
 });
 
+router.get("/brandWithLessModels", async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile(fileName));
+    let results = [];
+    data.sort((a, b) => a.models.length - b.models.length);
+    data.forEach((record) => {
+      if (results.length === 0) {
+        results.push(record);
+      } else {
+        results.forEach((result) => {
+          if (result.models.length === record.models.length) {
+            results.push(record);
+          }
+        });
+      }
+    });
+    res.send(
+      results.map(function (element) {
+        return element.brand;
+      })
+    );
+  } catch (err) {
+    next(err);
+  }
+});
+
 router.use((err, req, res, next) => {
   logger.error(`${req.method} ${req.baseUrl} - ${err.message}`);
   res.status(400).send({ error: err.message });
